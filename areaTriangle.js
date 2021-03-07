7ca33754d43a756ffe5a2977d11718c745b90af1
lemansali <79211103+lemansali@users.noreply.github.com>
2021-03-07 20:58:58 +0200

    Create areaTriangle.js

---
+++ areaTriangle.js
@@ -0,0 +1,10 @@
+function area(arr) {
+    let side1 = Number(arr[0]);
+    let side2 = Number(arr[1]);
+    let side3 = Number(arr[2]);
+
+    let sum = (side1 + side2 + side3)/2;
+    
+    console.log(sum)
+}
+area(['3', '4', '5']);
